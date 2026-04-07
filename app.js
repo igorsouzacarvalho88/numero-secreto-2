a3c0912ddab0b2c07ce777f3ea4b1575d8f72a17
Igor Souza Carvalho <igmalte2@gmail.com>
2026-04-07 14:28:07 -0300

    Criando função para verificar se houve chute

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 let titulo = document.querySelector('h1');
 titulo.innerHTML = ('Jogo do número secreto');
+
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = ('Tente advinhar o número secreto entre 1 e 100');
+
+function verificarChute(){
+    console.log('Verificando o chute');
+    let chute = document.querySelector('#chute').value;
+}
