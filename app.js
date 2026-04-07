cdf32ef2bb747ef24f989f6adbf28382c96c31b6
Igor Souza Carvalho <igmalte2@gmail.com>
2026-04-07 16:19:48 -0300

    dando retorno booleano para confirmar o número secreto

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ exibirTextoTela('p', 'Tente advinhar o número secreto entre 1 e 10');
 
 
 function verificarChute(){
+    let chute = document.querySelector('input').value;
     console.log(numeroSecreto);
+    console.log(chute == numeroSecreto);
 }
 
 function gerarNumeroAleatorio(){
