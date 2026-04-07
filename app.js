8190df4cae20b105294afa7f16be0abac88169f5
Igor Souza Carvalho <igmalte2@gmail.com>
2026-04-07 14:19:34 -0300

    Creando texto para o jogo

---
+++ app.js
@@ -0,0 +1,4 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = ('Jogo do número secreto');
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = ('Tente advinhar o número secreto entre 1 e 100');
